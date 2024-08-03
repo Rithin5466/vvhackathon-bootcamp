ae96647c2fafe067ce7c9f8e30732b3ee59c4e38
Rithin5466 <chegondirithinsurya@gmail.com>
2024-08-03 23:36:37 +0530

    vvhack

---
+++ tasks/sample.js
@@ -0,0 +1,68 @@
+// let a="10";
+// console.log(a);
+// let b=20;
+// console.log(b);
+// let Object={  name:"rithin",
+// b:"hii"}
+// console.log(Object.name)
+// arr=[1,"rithin",123,{key:"hello"}];
+// console.log(arr);
+// console.log(typeof(arr[3]));
+// let date={
+//     name:"rithin",
+//     car:"audi",
+//     num:1234,
+// }
+// console.log(date.name&&"data found");   
+// console.log(date.name||"data found"); 
+// console.log(date.name&&"data found"); 
+// console.log(date.name?date.name:"false");   
+// console.log(!!(date.name))
+// if(!date.name)
+//     {
+//         console.log(date.name);
+//     }
+//     else (date.car)
+//         {
+//             console.log(date.car);
+//         }
+//        let num=3
+//         switch(num){
+//             case 1:
+//                 console.log(date.name);
+//                 break;
+//                 case 2:
+//                     console.log(date.car);
+//                     break;
+//                     default:
+//                         console.log("no match");
+//         }
+// let toogle={
+//     one:console.log(date.name),
+//     two:console.log(date.car),
+
+
+//     default:console.log("no match"),
+
+// }
+// toogle.two;
+//arra
+// let arr=[1,2,34,5,67]
+// for(i=0;i<5;i++)(
+//     console.log(arr[i])
+// )
+
+// for (let x of arr){
+//     console.log(x)
+// }
+// for (let x in arr){
+//     console.log(x)
+// }
+// for (let x in arr){
+//     console.log(x,arr[x])
+// }
+let i=1
+while(i<5){
+    console.log(i)
+    i++
+}
